007e0a1c480e4f89846d2f197829e092a8b3e4c6
Y.Matsuda <ymatsuda.zck@gmail.com>
2026-03-11 12:54:54 +0900

    feat(slack): improve description for the bot_token param

--- src/slack-connector/main.go
+++ src/slack-connector/main.go
@@ -18,7 +18,7 @@ func GetConfigSchema() (ConfigSchema, error) {
 			"bot_token": map[string]any{
 				"type":        "string",
 				"title":       "Bot User OAuth Token",
-				"description": "Slack Bot Token (xoxb-...)",
+				"description": "User OAuth Token (xoxp-...)",
 			},
 			"user_id": map[string]any{
 				"type":        "string",
